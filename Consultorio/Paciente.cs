4a2aad19abd22009e130d8e9db8ddc5226135d4e
Paulo Souza <paulo.souza@dcx.ufpb.br>
2024-06-08 09:30:11 -0300

    Classe de paciente implementada

---
+++ Consultorio/Paciente.cs
@@ -0,0 +1,116 @@
+namespace Consultorio;
+
+public class Paciente
+{
+    public string CPF { get; private set; }
+    public string Nome { get; private set; }
+    public DateTime DataNascimento { get; private set; }
+
+    public Paciente(string cpf, string nome, string dataNascimento)
+    {
+        SetCPF(cpf);
+        SetNome(nome);
+        SetDataNascimento(dataNascimento);
+    }
+    private void SetCPF (string cpf){
+        if (CpfValido){
+            CPF = cpf;
+        }else {
+            throw new ArgumentException("CPF Inválido");
+        }
+    }
+    private void SetNome (string nome){
+        if (nome.Length >= 5){
+            Nome = nome
+        }else{
+            throw new ArgumentException("Nome deve ter pelo menos 5 caracteres.");
+        }
+    }
+    private void SetDataNascimento(string dataNascimento)
+    {
+        if (DateTime.TryParseExact(dataNascimento, "ddMMyyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime data))
+        {
+            if (CalcularIdade(data) >= 13)
+            {
+                DataNascimento = data;
+            }
+            else
+            {
+                throw new ArgumentException("Paciente deve ter 13 anos ou mais.");
+            }
+        }
+        else
+        {
+            throw new ArgumentException("Data de nascimento inválida.");
+        }
+    }
+
+    private int CalcularIdade (DateTime dataNascimento){
+        int idade = DateTime.Now.Yeat - dataNascimento.Year;
+        if (DateTime.Now.DayOfYear < dataNascimento.DayOfYear){
+            idade--;
+        }
+        return idade
+    }
+    private bool CpfValido(string cpf)
+    {
+        cpf = Regex.Replace(cpf, @"[^\d]", "");
+
+        if (cpf.Length != 11)
+        {
+            return false;
+        }
+
+        for (int j = 0; j < 10; j++)
+        {
+            if (cpf == new string((char)('0' + j), 11))
+            {
+                return false;
+            }
+        }
+
+        int[] multiplicadores1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        int[] multiplicadores2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+        string tempCpf = cpf.Substring(0, 9);
+        int soma = 0;
+
+        for (int i = 0; i < 9; i++)
+        {
+            soma += int.Parse(tempCpf[i].ToString()) * multiplicadores1[i];
+        }
+
+        int resto = soma % 11;
+        if (resto < 2)
+        {
+            resto = 0;
+        }
+        else
+        {
+            resto = 11 - resto;
+        }
+
+        string digito = resto.ToString();
+        tempCpf = tempCpf + digito;
+        soma = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            soma += int.Parse(tempCpf[i].ToString()) * multiplicadores2[i];
+        }
+
+        resto = soma % 11;
+        if (resto < 2)
+        {
+            resto = 0;
+        }
+        else
+        {
+            resto = 11 - resto;
+        }
+
+        digito = digito + resto.ToString();
+
+        return cpf.EndsWith(digito);
+    }
+}
